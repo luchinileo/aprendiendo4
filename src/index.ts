25063d6e6911614387c7b99e6ac5dcbce4010152
luchinileo <104324324+luchinileo@users.noreply.github.com>
2022-05-25 21:53:50 -0300

    cambiar el texto

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 let nota, suma, promedio, contador: number;
 suma = 0;
-for (let contador: number = 1; contador < 11; contador++) {
+  let NombreDelAlumno:string="Juan"
+
+for (let contador: number = 1; contador < 3; contador++) {
   nota = Number(prompt("Ingrese la nota" + contador + ":"));
   suma += nota;
 }
-promedio = suma / 10;
-console.log("El promedio de las notases:" + promedio);
+contador=contador+1;  
+promedio = suma / 3;
+console.log("El promedio de las notas es:" + promedio);
+
+
+
+   let nota1NumberLaPartePracticaVale: 10%;
+   let nota2LaParteeProblemasVale:50%;
+   let nota3laparteteoricaVael:number= 40%;
+
